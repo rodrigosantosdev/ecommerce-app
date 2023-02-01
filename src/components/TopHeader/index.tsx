33b02af52ef67e783df433497dd8013e2f037800
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-02-01 14:59:27 -0300

    adjust compoent TopHeader

--- src/components/TopHeader/index.tsx
+++ src/components/TopHeader/index.tsx
@@ -1,24 +1,11 @@
-import { 
-  TiSocialLinkedin, 
-  TiSocialFacebook, 
-  TiSocialInstagram, 
-  TiSocialTwitter, 
-  TiSocialPinterestCircular   
-} from 'react-icons/ti'
-
-import { TopHeaderContainer, TopHeaderContent, TopHeaderInfo, TopHeaderSocial } from "./style"
+import SocialLinks from "../SocialLinks"
+import { TopHeaderContainer, TopHeaderContent, TopHeaderInfo } from "./style"
 
 export default function TopHeader() {
   return (
     <TopHeaderContainer>
       <TopHeaderContent>
-        <TopHeaderSocial>
-          <li><a href="#"><TiSocialFacebook size={18} /></a></li>
-          <li><a href="#"><TiSocialInstagram size={18} /></a></li>
-          <li><a href="#"><TiSocialTwitter size={18} /></a></li>
-          <li><a href="#"><TiSocialLinkedin size={18} /></a></li>
-          <li><a href="#"><TiSocialPinterestCircular size={18} /></a></li>
-        </TopHeaderSocial>
+        <SocialLinks />
         <TopHeaderInfo>
           <a href="#">Sobre</a>
           <a href="#">Fale conosco</a>
--- src/components/TopHeader/style.ts
+++ src/components/TopHeader/style.ts
@@ -15,30 +15,6 @@ export const TopHeaderContent = styled.div`
   gap: 0.875rem;
 `
 
-export const TopHeaderSocial = styled.div`
-  display: flex;
-  gap: 0.675rem;
-
-  li a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-
-    width: 28px;
-    height: 28px;
-
-    cursor: pointer;
-    border: 1px solid var(--rgba);
-    border-radius: 4px;
-    color: black;
-
-    &:hover {
-      background-color: var(--base-color);
-      color: var(--base-color-dark);
-    }
-  }
-`
-
 export const TopHeaderInfo = styled.div`
 
   display: flex;
@@ -49,7 +25,7 @@ export const TopHeaderInfo = styled.div`
     font-size: 0.875rem;
     
     &:hover {
-      color: var(--base-color);
+      color: var(--base);
     }
   }
 
