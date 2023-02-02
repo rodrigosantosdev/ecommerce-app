fb35ecf216ce4034298179e71277c9308b9368a5
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-02-02 18:23:25 -0300

    adjust styles

---
+++ src/components/Cart/index.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react"
+import { BsHandbag } from 'react-icons/bs'
+import CartSideBar from "../CartSidebar"
+import { CartButton, CartContainer, CartIcon, CartText } from "./style"
+
+export default function Cart() {
+
+  const [showSidebar, setShowSidebar] = useState(true)
+
+  const showOpenSidebar = () => setShowSidebar(!showSidebar)
+
+  return (
+    <CartContainer>
+      <CartButton onClick={showOpenSidebar}>
+        <CartIcon>
+          <BsHandbag size={24} />
+        </CartIcon>
+        <CartText>
+          <p>Sacola</p>
+        </CartText>
+        {showSidebar && <CartSideBar />}
+      </CartButton>
+    </CartContainer>
+  )
+}
--- src/components/Cart/style.ts
+++ src/components/Cart/style.ts
@@ -1,19 +1,19 @@
 import styled from "styled-components";
 
+export const CartContainer = styled.div``
 
-export const CartContainer = styled.div`
+export const CartButton = styled.button`
   display: flex;
-  align-items: end;
+  align-items: center;
   gap: 1rem;
+  padding: 0.75rem;
+  background-color: transparent;
+  cursor: pointer;
 `
 
 export const CartIcon = styled.div``
 
 export const CartText = styled.div`
   font-size: 14px;
-
-  span {
-    font-weight: bold;
-  }
-
+  font-weight: bold;
 `
